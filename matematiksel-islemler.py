feb9790c99642c716b51f1ca675b84b1035bcad3
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-10-01 11:57:33 +0300

    matematiksel işlemler oluşturuldu.

---
+++ matematiksel-islemler.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+v = np.array([1,2,3,4,5])
+
+# ufunc
+
+print(np.subtract(v,1))
+
+print(np.add(v,3))
+
+print(np.multiply(v,3))
+
+print(np.divide(v,3))
+
+print(np.power(v,2))
+
+print(np.power(v,3))
+
+print(np.mod(v,2))
+
+print(np.absolute(np.array([-3])))
